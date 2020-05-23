48262a47e4bd7ebd68a3b7ff1e03dab5f48bac56
devthet <devthet@gmail.com>
2020-05-23 22:12:19 +0630

    Finish product

--- app/src/main/java/com/example/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/ProductsActivity.kt
@@ -1,16 +1,37 @@
 package com.example.coderswag.controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.coderswag.R
+import com.example.coderswag.adapters.ProductAdapter
+import com.example.coderswag.services.DataService
 import com.example.coderswag.utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter:ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
        var categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+       adapter = ProductAdapter(this,DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation== Configuration.ORIENTATION_LANDSCAPE){
+        spanCount = 3
+        }
+        val screenSize = resources.configuration.screenHeightDp
+        if(screenSize>720){
+            spanCount = 3
+        }
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/coderswag/services/DataService.kt
+++ app/src/main/java/com/example/coderswag/services/DataService.kt
@@ -4,45 +4,58 @@ import com.example.coderswag.model.Category
 import com.example.coderswag.model.Product
 
 object DataService {
-    var categories = listOf(
-        Category("Shirts","shirtimage"),
-        Category("Hoodies","hoodieimage"),
-        Category("Hats","hatimage"),
+    val categories = listOf(
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
         Category("Digital","digitalgoodsimage"),
-        Category("Shirts","shirtimage"),
-        Category("Hoodies","hoodieimage"),
-        Category("Hats","hatimage"),
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
         Category("Digital","digitalgoodsimage"),
-        Category("Shirts","shirtimage"),
-        Category("Hoodies","hoodieimage"),
-        Category("Hats","hatimage"),
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
         Category("Digital","digitalgoodsimage"),
-        Category("Shirts","shirtimage"),
-        Category("Hoodies","hoodieimage"),
-        Category("Hats","hatimage"),
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
         Category("Digital","digitalgoodsimage"),
-        Category("Shirts","shirtimage"),
-        Category("Hoodies","hoodieimage"),
-        Category("Hats","hatimage"),
+        Category("SHIRTS","shirtimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
         Category("Digital","digitalgoodsimage")
 
     )
 
-    var hats = listOf(
+    val hats = listOf(
+        Product("Devslope graphic Beanie","$18","hat1"),
+        Product("Devslope Hat Black","$20","hat2"),
+        Product("Devslope Hat White","$18","hat3"),
+        Product("Devslope Hat Snapback","$18","hat4"),
         Product("Devslope graphic Beanie","$18","hat1"),
         Product("Devslope Hat Black","$20","hat2"),
         Product("Devslope Hat White","$18","hat3"),
         Product("Devslope Hat Snapback","$18","hat4")
 
     )
-    var hoodies = listOf(
+    val hoodies = listOf(
     Product("Devslope Hoodie Gray","$28","hoodie1"),
     Product("Devslope Hoodie Black","$20","hoodie2"),
     Product("Devslope Hoodie White","$18","hoodie3"),
-    Product("Devslope Hoodie Snapback","$18","hoodie4")
+    Product("Devslope Hoodie Snapback","$18","hoodie4"),
+        Product("Devslope Hoodie Gray","$28","hoodie1"),
+        Product("Devslope Hoodie Black","$20","hoodie2"),
+        Product("Devslope Hoodie White","$18","hoodie3"),
+        Product("Devslope Hoodie Snapback","$18","hoodie4")
 
     )
-    var shirts = listOf(
+    val shirts = listOf(
+        Product("Devslope Shirt Gray","$28","shirt1"),
+        Product("Devslope Shirt Black","$20","shirt2"),
+        Product("Devslope Shirt White","$18","shirt3"),
+        Product("Devslope Shirt Snapback","$18","shirt4"),
+        Product("Kickflip Shirt ","$18","shirt5"),
         Product("Devslope Shirt Gray","$28","shirt1"),
         Product("Devslope Shirt Black","$20","shirt2"),
         Product("Devslope Shirt White","$18","shirt3"),
@@ -50,4 +63,16 @@ object DataService {
         Product("Kickflip Shirt ","$18","shirt5")
 
     )
+    val dititalGood = listOf<Product>()
+
+    fun getProducts(category:String): List<Product>{
+        return  when(category){
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> dititalGood
+        }
+
+
+    }
 }
